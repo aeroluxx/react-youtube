aede9844e57dc2300d72e2cc0bd654eb9c6f5dc8
aero <aeroluxx@mail.ru>
2019-07-07 15:07:16 +0300

    second

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoList from './Todo/TodoList'
 function App() {
   return (
     <div className="wrapper">
-      <h1>React tutorial</h1>
+      <h1>React tutorial---</h1>
       <TodoList />
     </div>
   )
